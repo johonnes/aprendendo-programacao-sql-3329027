835af240b5737afe877580f1a5c86c875936ba5a
johonnes <167844970+johonnes@users.noreply.github.com>
2024-05-01 10:19:47 +0000

    RESOLUÇÃO 2 EXERCICIO

--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,8 @@
--- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
-
+-- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caract
+CREATE TABLE SALARIO_CLASS(
+ID_SALARIO INTEGER PRIMARY KEY AUTOINCREMENT,
+NIVEL VARCHAR(50),
+SALARIO INTEGER);
 -- Na tabela employees, adicione uma coluna para receber o id_salario
+ALTER TABLE employees
+DROP PRIMARY KEY;
